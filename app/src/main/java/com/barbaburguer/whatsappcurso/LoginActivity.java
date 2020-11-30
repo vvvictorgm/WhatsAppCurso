ce1ae0fd2f04632d2f798e29c4007bd87399b7eb
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2020-11-30 14:20:09 -0300

    adicionados Mascaras
    Adicionado Mascara em Código de Pais, área, e telefone

--- app/src/main/java/com/barbaburguer/whatsappcurso/LoginActivity.java
+++ app/src/main/java/com/barbaburguer/whatsappcurso/LoginActivity.java
@@ -3,12 +3,37 @@ package com.barbaburguer.whatsappcurso;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.github.rtoshiro.util.format.SimpleMaskFormatter;
+import com.github.rtoshiro.util.format.text.MaskTextWatcher;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private EditText telefone,codPais,codArea,nome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        nome = findViewById(R.id.editNome);
+        telefone = findViewById(R.id.editTelefone);
+        codArea = findViewById(R.id.edit_code_area);
+        codPais = findViewById(R.id.edit_cod_pais);
+
+        //colocando mascaras
+        //Codigo Pais
+        SimpleMaskFormatter simpleMaskPais = new SimpleMaskFormatter("+NN");
+        MaskTextWatcher maskPais = new MaskTextWatcher(codPais, simpleMaskPais);
+        codPais.addTextChangedListener(maskPais);
+
+        //Codigo de Area
+        SimpleMaskFormatter simpleMaskArea = new SimpleMaskFormatter("(NN) ");
+        MaskTextWatcher maskArea = new MaskTextWatcher(codArea, simpleMaskArea);
+        codArea.addTextChangedListener(maskArea);
+
+        //telefone
+        SimpleMaskFormatter simpleMaskTelefone = new SimpleMaskFormatter("NNNNN-NNNN");
+        MaskTextWatcher maskTelefone = new MaskTextWatcher(telefone, simpleMaskTelefone);
+        telefone.addTextChangedListener(maskTelefone);
     }
 }
